511b2735c2c960a0cebf3481cb5436e9aa280293
Эверстова Нина <ninaever@mail.ru>
2022-07-21 21:53:37 +0900

    query

---
+++ src/main/java/ru/netology/Handler.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+import java.io.BufferedOutputStream;
+
+@FunctionalInterface
+public interface Handler {
+
+    public void handle(Request request, BufferedOutputStream out);
+}
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,9 +1,74 @@
 package ru.netology;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Main {
+
+    private static final List<String> pathsList = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/events.html", "/events.js", "/default-get.html");
 
     public static void main(String[] args) {
         final var server = new Server();
+        pathsList.forEach(x -> server.addHandler("GET" + x, ((request, out) -> {
+            try {
+                final var type = Files.probeContentType(request.getPath());
+                final var length = Files.size(request.getPath());
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + type + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(request.getPath(), out);
+                out.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        })));
+        server.addHandler("GET/classic.html", ((request, out) -> {
+            try {
+                final var type = Files.probeContentType(request.getPath());
+                final var template = Files.readString(request.getPath());
+                final var content = template.replace("{time}", LocalDateTime.now().toString()).getBytes(StandardCharsets.UTF_8);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + type + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }));
+        server.addHandler("GET/forms.html", ((request, out) -> {
+            if (request.getQuery().isEmpty()) {
+                try {
+                    final var type = Files.probeContentType(request.getPath());
+                    final var length = Files.size(request.getPath());
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + type + "\r\n" +
+                                    "Content-Length: " + length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    Files.copy(request.getPath(), out);
+                    out.flush();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                System.out.println(request.getQuery());
+            }
+        }));
+        server.addHandler("POST/default-get.html", ((request, out) -> System.out.println(request.getBody())));
         server.start(9999);
     }
 }
